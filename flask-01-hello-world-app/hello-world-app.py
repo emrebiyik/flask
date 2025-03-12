3c79f0800410be7dd156eec60b00cfe8e576831f
emrebiyik <emrebiyik34@gmail.com>
2025-03-12 12:01:37 +0100

    edit flask1

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -1,7 +1,7 @@
 from flask import Flask 
 app = Flask(__name__)
 
-app.route('/')
+@app.route('/')
 def head():
    return "Hello World Paul"
 
@@ -22,3 +22,4 @@ if __name__ == '__main__':
 
      # app.run(debug=True)
      app.run(host= '0.0.0.0', port=8080)
+     
