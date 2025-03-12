bbeb49050e18dd6471ce894438a91f3643a46410
emrebiyik <emrebiyik34@gmail.com>
2025-03-12 11:54:49 +0100

    added flask1-2

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -1,18 +1,24 @@
 from flask import Flask 
 app = Flask(__name__)
-@app.route('/')
 
+app.route('/')
+def head():
+   return "Hello World Paul"
 
-@app.route('/secondpage')
-
+@app.route('/second')
+def second():
+   return "This is second Page"
 
 @app.route('/third')
-
+def third():
+   return "This is third Page"
 
 @app.route('/fourth/<string:id>')
+def fourth(id):
+   return f'Id of this page {id}'
 
 
 if __name__ == '__main__':
 
      # app.run(debug=True)
-     app.run(host= '0.0.0.0', port=80)
+     app.run(host= '0.0.0.0', port=8080)
